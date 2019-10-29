d94d8c1d62856fdf57eb9aa5481f4fb4bee6bb16
pwyoung <phil.w.young@gmail.com>
2019-10-29 14:55:13 -0400

    noisy vscode

--- generic/dot_profile.d/python.sh
+++ generic/dot_profile.d/python.sh
@@ -5,6 +5,9 @@ VENV_DIR=~/venv/python3
 if [ ! -d ${VENV_DIR} ]; then
     echo "Creating venv dir ${VENV_DIR}"
     python3 -m venv ${VENV_DIR}
+
+    export set PYTHONIOENCODING=UTF-8
+    export set PYTHONUNBUFFERED=1
 fi
 
 source ${VENV_DIR}/bin/activate
