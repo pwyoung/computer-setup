b6a97c849fb93efa99c50fc0929771f23b08c4e6
pwyoung <phil.w.young@gmail.com>
2019-12-03 15:10:05 -0500

    wip

--- home/.profile.d/editor_and_alias.sh
+++ home/.profile.d/editor_and_alias.sh
@@ -4,10 +4,11 @@
 #   for op in start enable status; do systemctl --user $op emacs; done
 
 
-if command -v flatpak 2>/dev/null; then
-    export EDITOR='emacs'
-    alias e='flatpak run org.gnu.emacs -nw 2>/dev/null'
-elif command -v emacsclient >/dev/null && systemctl --user status emacs >/dev/null; then
+#if command -v flatpak 2>/dev/null; then
+#    export EDITOR='emacs'
+#    alias e='flatpak run org.gnu.emacs -nw 2>/dev/null'
+#el
+if command -v emacsclient >/dev/null && systemctl --user status emacs >/dev/null; then
     export EDITOR='emacsclient'
     alias e='emacsclient -nw -a emacs'
 else
