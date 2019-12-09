9e12d6e9cba645b0aca66ac23ea1922a55c1debe
pwyoung <phil.w.young@gmail.com>
2019-12-08 21:08:36 -0500

    Fixed python.sh

--- home/.profile.d/python.sh
+++ home/.profile.d/python.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+
+echo "python.sh: start"
 VENV_DIR=~/venv/python3
 
 if [ ! -d ${VENV_DIR} ]; then
     echo "Creating venv dir ${VENV_DIR}"
     python3 -m venv ${VENV_DIR}
-
-    export set PYTHONIOENCODING=UTF-8
-    export set PYTHONUNBUFFERED=1
 fi
 
+export set PYTHONIOENCODING=UTF-8
+export set PYTHONUNBUFFERED=1
+
 source ${VENV_DIR}/bin/activate
+
+echo "python.sh: end"
