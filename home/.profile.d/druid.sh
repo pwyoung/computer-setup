bfe11a351aab9befb2b03468b6d4988aac827ef1
pwyoung <phil.w.young@gmail.com>
2020-02-09 15:47:07 -0500

    wip

---
+++ home/.profile.d/druid.sh
@@ -0,0 +1,2 @@
+DRUID_JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export DRUID_JAVA_HOME
--- ubuntu/golang/install.sh
+++ ubuntu/golang/install.sh
@@ -1,1 +1,9 @@
+
+# install Go and set GOPATH
 sudo snap install --classic go
+
+go version
+bash -c 'env | grep GO'
+
+# https://www.tecmint.com/install-go-in-linux/
+# mkdir -p ~/go_projects/{bin,src,pkg}
