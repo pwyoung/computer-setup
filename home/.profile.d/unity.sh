02c6eb4bc4627b4b04ebbfb25707c19b45034f07
pwyoung <phil.w.young@gmail.com>
2021-05-29 19:24:22 -0400

    update unity aliases.

--- home/.profile.d/unity.sh
+++ home/.profile.d/unity.sh
@@ -1,6 +1,9 @@
 alias u='cd ~/unity; ./UnityHub.AppImage'
+alias unity='cd ~/unity; ./UnityHub.AppImage'
 
-# Go to the dir where unity puts downloaded projects
+# Remember some useful things and make them useful as aliases
 if uname | grep Linux; then
-    alias _unity_dir='cd /home/pwyoung/.local/share/unity3d/Asset\ Store-5.x/Unity\ Technologies/Project'
+    alias _unity_cli='/home/pwyoung/Unity/Hub/Editor/2020.3.10f1/Editor/Unity'
+    alias _unity_log='cat /home/pwyoung/.config/unity3d/Editor.log'
+    alias _unity_cd_project='cd /home/pwyoung/.local/share/unity3d/Asset\ Store-5.x/Unity\ Technologies/Project'
 fi
