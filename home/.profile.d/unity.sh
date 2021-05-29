02ea719d1b81f1d25cb18be00c3131a8b7ecb76f
pwyoung <phil.w.young@gmail.com>
2021-05-29 15:11:49 -0400

    Add unity aliases

--- home/.profile.d/unity.sh
+++ home/.profile.d/unity.sh
@@ -1,2 +1,6 @@
-alias unity='cd ~/unity; ./UnityHub.AppImage'
-alias u=unity
+alias u='cd ~/unity; ./UnityHub.AppImage'
+
+# Go to the dir where unity puts downloaded projects
+if uname | grep Linux; then
+    alias _unity_dir='cd /home/pwyoung/.local/share/unity3d/Asset\ Store-5.x/Unity\ Technologies/Project'
+fi
