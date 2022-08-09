278755f402c2153c33a377edc09716d562c8c72f
phillip young <phil.young@insight.com>
2022-08-09 19:07:30 -0400

    do not use podman on mac

--- home/.profile.d/podman.sh
+++ home/.profile.d/podman.sh
@@ -1,7 +1,14 @@
-if command -v podman >/dev/null; then
-    alias docker='podman'
+#!/bin/bash
+
+# Do not run podman on mac
+if ! uname | grep Darwin >/dev/null; then
+
+    if command -v podman >/dev/null; then
+        alias docker='podman'
+    fi
+
+    if command -v podman-compose >/dev/null; then
+        alias docker-compose='podman-compose'
+    fi
 fi
 
-if command -v podman-compose >/dev/null; then
-    alias docker-compose='podman-compose'
-fi
