76fdb77173976fd949d175d48c419351f0a84a16
phillip young <phil.young@insight.com>
2022-09-12 17:10:26 -0400

    fix nomaj setup

--- home/.profile.d/nomaj.sh
+++ home/.profile.d/nomaj.sh
@@ -1,6 +1,11 @@
-echo "Set up nomaj"
-NOMAJ_HOME=/home/$USER/git/nomaj
+L=/tmp/nomaj-setup.txt
+echo "start: `date`" > $L
+
+NOMAJ_HOME=$HOME/git/nomaj
 if [ -e $NOMAJ_HOME ]; then
     export PATH=$PATH:$NOMAJ_HOME
+    echo "Found nomaj. Set PATH=$PATH" >> $L
 fi
+echo "end: `date`" >> $L
+
 
