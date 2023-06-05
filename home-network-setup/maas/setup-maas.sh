bf7238a0e7c9a9d607d8baa948127fe539423834
phillip young <phil.w.young@gmail.com>
2023-06-04 22:35:22 -0400

    wip: automatic. TODO: rebase this

--- home-network-setup/maas/setup-maas.sh
+++ home-network-setup/maas/setup-maas.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+# Temporary creds
+# MAAS_API_KEY
+#   - pwy-maas-apikey
+#     - pEKP2f728qJE6WQ827:WCeJaLMCLj9ZTuFCJF:8GXfauJjj8QZXJpF3xxfS9Z6mD5KfXpE
+
 install-maas() {
     if which maas >/dev/null; then
 	echo "Maas is in PATH. Assuming it is installed properly."
@@ -33,37 +38,37 @@ setup-admin() {
 	# email: notused@gmail.com
 	# gh:pwyoung
     fi
-    
+
     IP=$(hostname)
     xdg-open http://${IP}:5240/MAAS
 }
 
 setup-networks() {
     # https://maas.io/docs/how-to-connect-maas-networks
-    cat <<EOF 
-    The default network, 'fabric0' should be created from the local subnet	
+    cat <<EOF
+    The default network, 'fabric0' should be created from the local subnet
 
 EOF
-   
+
 }
 
 setup-dhcp() {
     # https://maas.io/docs/how-to-enable-dhcp
-    cat <<EOF 
+    cat <<EOF
     Choose the default VLAN. Click on Maas -> subnets -> untagged
     My setup:
     Subnet: 192.168.8.0/24
-    Mangement: 
+    Mangement:
       - by default, the subnet is managed (i.e. Maas will provide DHCP)
       - To change the management of a subnet, see:
         - https://discourse.maas.io/t/subnet-management-deb-3-0-cli-test/4716#heading--controlling-subnet-management
     IP Range: 192.168.8.100 - 192.168.8.200 (using static IPs in low range, for the Maas server itself)
 
-    Review: 
+    Review:
       - https://discourse.maas.io/t/how-to-manage-ip-ranges/5136
-      - https://discourse.maas.io/t/maas-glossary/5416#heading--ip-ranges 
+      - https://discourse.maas.io/t/maas-glossary/5416#heading--ip-ranges
       - https://discourse.maas.io/t/subnet-management-deb-3-0-cli-test/4716
-      - https://discourse.maas.io/t/subnet-management-deb-3-0-cli-test/4716#heading--controlling-subnet-management 
+      - https://discourse.maas.io/t/subnet-management-deb-3-0-cli-test/4716#heading--controlling-subnet-management
 
 
 EOF
@@ -76,11 +81,33 @@ test-dhcp() {
     F=/tmp/dhcpcd.out
     sudo dhcpcd -T enp8s0 -t 2 &> $F #| tee $F
     echo "Scanning results of DHCP test at $F"
-    echo "Look at the file via: cat $F"   
+    echo "Look at the file via: cat $F"
     if cat $F | grep 'new_filename' | grep 'lpxelinux.0' >/dev/null; then
 	echo "Looks good"
     fi
 
+}
+
+setup-lxd-host() {
+    cat <<EOF
+    https://maas.io/docs/how-to-set-up-lxd
+
+    Created default everything, but
+    storage pool has type "dir" (per docs above)
+    no connection to maas automatically
+    yes, expose to network
+
+    # Do not add another DHCP or DNS server
+    lxc network set lxdbr0 dns.mode=none
+    lxc network set lxdbr0 ipv4.dhcp=false
+    lxc network set lxdbr0 ipv6.dhcp=false
+
+    # IP: 192.168.8.100 (DHCP)
+    # LXC bridge
+    #  lxc network show lxdbr0
+    # 10.25.155.1
+
+EOF
 }
 
 check-status
