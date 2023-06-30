3b52ec1d0e17036fa8c69d9902e68c054a361d82
phillip young <phil.young@insight.com>
2023-06-29 21:37:15 -0400

    wip: automatic. TODO: rebase this

---
+++ home/bin/_segregate_files_by_day_modified.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Goal: put all files in the local dir into subdirs, where
+# each subdir is named for the (modification) date of the file.
+
+# Credit https://unix.stackexchange.com/questions/683425/how-to-move-files-into-subdirectories-segregated-by-date
+# This is not efficient, since it runs mkdir for each file, but it is easily maintainable.
+
+for file in *; do
+    [ ! -L "$file" ] &&
+      dir_name=$(date -r "$file" +%Y-%m-%d) &&
+      mkdir -p "$dir_name" &&
+      mv -- "$file" "$dir_name"
+done
+
+
