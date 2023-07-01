5a8b79c8cdcdf7e5c40e5b8619db893e58a28307
phillip young <phil.young@insight.com>
2023-07-01 00:00:08 -0400

    Allow commands to run in the dev container

--- home/.profile.d/docker-dev-tools.sh
+++ home/.profile.d/docker-dev-tools.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-D=~/git/docker-dev-tooling/bin
-if [ -e $D ]; then
-    PATH=$D:$PATH
-    export $PATH
+L=~/docker-dev-tools.sh.out
+
+H=$(hostname)
+
+# If the hostname starts with 'devc-' then assume we are in a dev container
+if [[ "$H" == "devc-"* ]]; then
+    echo "In dev container" > $L
+else
+    echo "NOT in dev container" > $L
+    D=~/git/docker-dev-tooling/bin
+    if [ -e $D ]; then
+        echo "Adding ~/git/docker-dev-tooling/bin to PATH" >> $L
+        PATH=$D:$PATH
+        export $PATH
+    fi
 fi
