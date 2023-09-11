12bfbb1bba9b59add91948003c917abcd77622cc
phillip young <phil.young@insight.com>
2023-09-11 16:45:45 -0400

    wip: automatic. TODO: rebase this

--- home/.profile.d/docker-dev-tools.sh
+++ home/.profile.d/docker-dev-tools.sh
@@ -6,15 +6,22 @@ L=/tmp/docker-dev-tools.sh.out
 
 H=$(hostname)
 
-# If the hostname starts with 'devc-' then assume we are in a dev container
-if [[ "$H" == "devc-"* ]]; then
+# If the hostname is 'dev' then assume we are in a dev container
+if [[ "$H" == "dev" ]]; then
     echo "In dev container" > $L
 else
     echo "NOT in dev container" > $L
     D=~/git/docker-dev-tooling/bin
     if [ -e $D ]; then
-        echo "Adding ~/git/docker-dev-tooling/bin to PATH" >> $L
+        echo "Adding $D to PATH" >> $L
         PATH=$D:$PATH
-        export $PATH
     fi
+
+    D2=~/git/docker-dev-tooling/bin/subcommands
+    if [ -e $D2 ]; then
+        echo "Adding $D2 to PATH" >> $L
+        PATH=$D2:$PATH
+    fi
+
+    export $PATH
 fi
