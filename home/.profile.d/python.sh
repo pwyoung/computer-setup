63d4a3ee0e3fd7677a4828c1e0f73e1a3ebf696b
phillip young <phil.w.young@gmail.com>
2023-09-18 18:37:03 -0400

    wip: automatic. TODO: rebase this

--- home/.profile.d/python.sh
+++ home/.profile.d/python.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+LOG=/tmp/python.sh.out
+
+# Conda
+F=/opt/conda/etc/profile.d/conda.sh
+if [ -e $F ]; then
+    echo "Setup Conda" | tee $LOG
+    . $F
+else
+    echo "No Conda" | tee $LOG
+fi
+
+
+# Pyenv
 DOTPYENV="$HOME/.pyenv"
 if [ -e "$DOTPYENV" ]; then
+    echo "Setup pyenv" | tee $LOG
     export PATH="$DOTPYENV/bin:$PATH"
     eval "$(pyenv init --path)"
     eval "$(pyenv virtualenv-init -)"
 fi
-
-
 
