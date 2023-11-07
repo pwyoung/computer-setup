bd914f0ac78e8ac5d14b04e0bad989d7c0e79ad4
phillip young <phil.w.young@gmail.com>
2023-11-07 14:23:07 -0500

    wip: automatic. TODO: rebase this

--- home/.profile.d/docker-dev-tools.sh
+++ home/.profile.d/docker-dev-tools.sh
@@ -17,11 +17,5 @@ else
         PATH=$D:$PATH
     fi
 
-    D2=~/git/docker-dev-tooling/bin/subcommands
-    if [ -e $D2 ]; then
-        echo "Adding $D2 to PATH" >> $L
-        PATH=$D2:$PATH
-    fi
-
     export $PATH
 fi
--- home/bin/aws-nuke-dir/aws-nuke-test.sh
+++ home/bin/aws-nuke-dir/aws-nuke-test.sh
@@ -2,8 +2,9 @@
 LOG=./aws-nuke-test.log
 
 echo "This does not run with --no-dry-run"
-echo "so it will only show what it would delete"
-./aws-nuke -c ./aws-nuke-config.yml &> $LOG
+echo "so it will NOT DELETE anything. "
+echo "It willo nly show what it would delete"
+./aws-nuke -c ./aws-nuke-config.yml 2>&1 | tee $LOG
 
 
 echo "################################################################################"
