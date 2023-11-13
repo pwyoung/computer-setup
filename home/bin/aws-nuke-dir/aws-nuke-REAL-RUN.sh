eb6635e2f570072ef88db76a887fc267d000b5e0
phillip young <phil.w.young@gmail.com>
2023-11-13 16:05:46 -0500

    wip: automatic. TODO: rebase this

--- home/bin/aws-nuke-dir/aws-nuke-REAL-RUN.sh
+++ home/bin/aws-nuke-dir/aws-nuke-REAL-RUN.sh
@@ -1,7 +1,6 @@
-LOG=./aws-nuke-REAL-RUN.log
 
 echo "This WILL DELETE RESOURCES"
 read -p "Hit enter to continue (or ctrl-c to exit)"
-./aws-nuke -c ./aws-nuke-config.yml --no-dry-run 2>&1 | tee $LOG
+aws-nuke -c ./aws-nuke-config.yml --no-dry-run
 
 
--- home/bin/aws-nuke-dir/aws-nuke-test.sh
+++ home/bin/aws-nuke-dir/aws-nuke-test.sh
@@ -1,13 +1,6 @@
 
-LOG=./aws-nuke-test.log
-
 echo "This does not run with --no-dry-run"
 echo "so it will NOT DELETE anything. "
 echo "It willo nly show what it would delete"
-./aws-nuke -c ./aws-nuke-config.yml 2>&1 | tee $LOG
+aws-nuke -c ./aws-nuke-config.yml
 
-
-echo "################################################################################"
-echo "Here's what would be removed"
-echo "################################################################################"
-cat $LOG | grep 'would remove'
